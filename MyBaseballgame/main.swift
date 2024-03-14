21852514aecd336d0f02a24bae5a3c26bd5a4943
hyelim9300 <hye930011@naver.com>
2024-03-14 14:08:15 +0900

    수정

--- MyBaseballgame/main.swift
+++ MyBaseballgame/main.swift
@@ -1,21 +1,40 @@
-let game = BaseballGame()
-game.start()
+import Foundation
 
 class BaseballGame {
+    private var answer = [Int]()
+    
     func start() {
-        let answer = makeAnswer()
+        answer = makeAnswer()
+        playGame()
     }
     
-    func makeAnswer() -> [Int] {
-        var numbers = [Int](1...9)
-        var gameAnswer = [Int]()
+    private func makeAnswer() -> [Int] {
+        var numbers = [Int](1...9)  // 1부터 9까지 Int값 선언
+        var gameAnswer = [Int]()  // 게임 정답값 선언
         
-            for _ in 0..<3 {
-                let index = Int.random(in: 0..<numbers.count)
-                gameAnswer.append(numbers[index])
-                numbers.remove(at: index)
-            }
-
-            return gameAnswer
+        for n in 0..<3 {  // n으로 인덱스값 설정 후 3가지까지 제한
+            let index = Int.random(in: 0...9)  // 랜덤 인덱스값
+            gameAnswer.append(numbers[index])  // 인덱스값 추가
+            numbers.remove(at: index)  // numbers 에서 인덱스에 있는 값 제외
         }
+        
+        return gameAnswer
+    }
+    
+    func playGame() {
+        print("3자리 숫자를 입력해주세요") // 게임 안내문
+        
+        while true {
+        guard let input = readLine(), // input 입력
+                let userInput = Int(input),
+                input.count == 3 else {
+            print("error")
+            continue // 잘못된 입력 시 에러, 반복
+        
+            if userInput == answer { // 정답 확인
+                            print("정답입니다")
+                            break // 정답을 맞췄으므로 반복 종료
+        }
+        
+    }
 }
