c2165638207fa6ff3c61ffc252564a4805bcae18
hyelim9300 <hye930011@naver.com>
2024-03-17 21:57:55 +0900

    튜터님 피드백 해석하고 너무 우울한 일요일

--- MyBaseballgame/main.swift
+++ MyBaseballgame/main.swift
@@ -18,6 +18,16 @@ class BaseballGame {
         }
         
         return gameAnswer                              // 정답 값 반환
+        
+//        ::성용 튜터님 피드백::
+//        var gameAnswer = [Int]()                     >> Int 타입 배열 정답 값 선언
+//        while gameAnswer.count < 3 {                 >> while 문으로 gameAnswer 이 3 전까지 수 순서? = (0, 1, 2) >> 즉 3개
+//            let number =  Int.random(in: 0...9)      >> Int 타입 넘버 값 선언, 0...9 에서 랜덤
+//            if !gameAnswer.contains(number) {        >> If 문 만약 정답 값 배열에 겹치는 수가 있을 경우 다시 넘버중에 랜덤 추가 없는 경우 추가 를 계속 반복해 배열의 수가 3개가 될때까지 하다가 멈추고 3개가 되면 값을 리턴하는 거구나~~~ 대박 너무 좋다.어케 이런 생각을 햇지 ㄷㄷ
+//                gameAnswer.append(number)            >>
+//            }
+//            return gameAnswer
+//        }
     }
     // MARK: --- lv. 1
     func playGame() {
@@ -42,7 +52,7 @@ class BaseballGame {
             }
             
             var strike = 0
-            var ball = 0                // 스트라이크, 볼 값 초기화
+            var ball = 0                                // 스트라이크, 볼 값 초기화
             
             for (index, value) in userNumbers.enumerated() {
                 if answer.contains(value) {
